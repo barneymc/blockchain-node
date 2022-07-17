4fafe1f31603cfa35214906fa2e68937931415a6
barneymc <brendan.mcardle@gmail.com>
2022-07-17 16:52:45 +0100

    Tried fixing the 'not a function' error

--- blockchain-node-api/contracts/Contacts.sol
+++ blockchain-node-api/contracts/Contacts.sol
@@ -1,10 +1,12 @@
 //pragma solidity ^0.8.10;
-pragma solidity >=0.4.22 <=0.8.15;
+//pragma solidity >=0.4.22 <=0.8.15;
+pragma solidity >=0.4.22 <=0.8.0;
 
 //import "truffle/Console.sol";
 
 
 contract Contacts {
+  
   uint public count = 1; // state variable
   //event Log(string message, uint val);
   event MyEvent(address indexed from, string message);
@@ -33,9 +35,9 @@ contract Contacts {
     contacts[count] = Contact(count, _name, _phone);
   }
 
- function getCount() public view returns (uint256){
-  return count;
- }
+ //function getCount() public view returns (uint256){
+ // return count;
+// }
 
   function updateCount() public {
     count++;
