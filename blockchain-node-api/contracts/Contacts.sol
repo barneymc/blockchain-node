8463ff074cb817833d2612dbcac2bbba7a7723c2
barneymc <brendan.mcardle@gmail.com>
2022-07-17 17:28:34 +0100

    Original Contract working again.

--- blockchain-node-api/contracts/Contacts.sol
+++ blockchain-node-api/contracts/Contacts.sol
@@ -2,16 +2,8 @@
 //pragma solidity >=0.4.22 <=0.8.15;
 pragma solidity >=0.4.22 <=0.8.0;
 
-//import "truffle/Console.sol";
-
-
 contract Contacts {
-  
-  uint public count = 1; // state variable
-  //event Log(string message, uint val);
-  event MyEvent(address indexed from, string message);
-
-
+  uint public count = 0; // state variable
 
   struct Contact {
     uint id;
@@ -20,27 +12,13 @@ contract Contacts {
   }
 
   constructor() public {
-    //console.log('In constructor');
-    createContact('Zafar Saleem', '12A');
-    //emit Log('In constructor', 23);
-    emit MyEvent(msg.sender, 'Just created a contract in Constructor()');
+    createContact('Zafar Saleem', '123123123');
   }
-
 
   mapping(uint => Contact) public contacts;
 
   function createContact(string memory _name, string memory _phone) public {
-   emit MyEvent(msg.sender, 'Just created a contract in createContract()');
     count++;
     contacts[count] = Contact(count, _name, _phone);
-  }
-
- //function getCount() public view returns (uint256){
- // return count;
-// }
-
-  function updateCount() public {
-    count++;
-    emit MyEvent(msg.sender, 'Increased count by 1');
   }
 }
