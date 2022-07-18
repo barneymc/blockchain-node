d5b9f57e803464027119f94da129367afb2b1dbd
barneymc <brendan.mcardle@gmail.com>
2022-07-18 16:55:00 +0100

    Added ABI (solcjs cli) and generated REST API on Kaleido

--- kaleido-io/truffle-config.js
+++ kaleido-io/truffle-config.js
@@ -7,8 +7,10 @@ module.exports = {
     development: {
       provider: () => {
         const appCred = 'yourappcred'; // from application credential widget
-        const connectionURL = 'nodeConnectionURL'; // without protocol (https://)        
-        return new HTTPProviderRateLimitRetry(`https://${appCred}@${connectionURL}`, 100000);
+        const connectionURL = 'e1rf5b2hzu-e1a0m1q1sl-rpc.eu1-azure.kaleido.io/'; // without protocol (https://)      
+        const newLink = 'https://e1ozsv1ync:EL6f5A5M3qMYRn0OsiYeJLzfd-eJY2HS_TbI8UCyn_o@e1rf5b2hzu-e1a0m1q1sl-rpc.eu1-azure.kaleido.io/';  
+//        return new HTTPProviderRateLimitRetry(`https://${appCred}@${connectionURL}`, 100000);
+        return new HTTPProviderRateLimitRetry(newLink, 100000);
       },
       network_id: "*", // Match any network id
       gasPrice: 0,
@@ -25,7 +27,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.7.0",
+      version: "0.8.15",
       settings: {
         evmVersion: "constantinople"
       }
